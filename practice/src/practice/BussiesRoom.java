449baae2e16819605ebca2520ac1835710ce05a3
ycastroa <mr.castro.y@gmail.com>
2022-04-11 01:45:10 -0400

    reduce time peek calls and add logic for best time

--- practice/src/practice/BussiesRoom.java
+++ practice/src/practice/BussiesRoom.java
@@ -7,6 +7,7 @@ public class BussiesRoom {
 	 * numRooms: 3 = K
 		Appointment times:     [1, 3, 5, 8, 19] => patients arrival time
 		Appointment durations: [20, 3, 2, 9, 1] => time spent by a patient in a given room
+		**expected output**
 		room 1 = 21
 		room 2 = 6,17
 		room 3 = 7, 20
@@ -20,9 +21,14 @@ public class BussiesRoom {
 			int end = times[i] +durations[i];
 			int currentBestRoom = 0;
 			boolean addNew = false;
+			int bestTime = 0;
+			if(!schedule.isEmpty())
+				bestTime = schedule.get(0).peek();				
 			for (int j = 0; j < rooms; j++) {
 				if(schedule.containsKey(j)){
-					if(schedule.get(j).peek()<end && currentBestRoom <= end) {
+					int duration = schedule.get(j).peek();
+					if(duration<end && bestTime >= duration ) {
+						bestTime = duration;
 						currentBestRoom = j;
 					}
 				}else {
